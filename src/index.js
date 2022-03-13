fd2c738f9eecb606d31f9903c0eee7e7305cbbca
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-13 22:13:59 +0900

    redux action

--- src/index.js
+++ src/index.js
@@ -2,8 +2,28 @@ import { createStore } from "redux";
 // const add = document.getElementById("add");
 // const minus = document.getElementById("minus");
 // const number = document.querySelector("span");
-const countModifier = () => {};
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    // console.log("they are telling me to add one");
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
 
 const countStore = createStore(countModifier);
 
-console.log(countStore);
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
