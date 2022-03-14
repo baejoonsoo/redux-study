f56865f1323e7e9640bf07baebcc369b726d3687
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-14 19:38:06 +0900

    react setup

--- src/index.js
+++ src/index.js
@@ -1,61 +1,6 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+import App from "./components/App";
 
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const reduser = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reduser);
-
-store.subscribe(() => console.log(store.getState()));
-
-const AddToDo = (text) => {
-  store.dispatch({ type: ADD_TODO, text });
-};
-
-const deleteToDo = (e) => {
-  store.dispatch({ type: DELETE_TODO, id: parseInt(e.target.parentNode.id) });
-};
-
-const paintToDo = () => {
-  const toDos = store.getState();
-
-  ul.innerHTML = "";
-
-  toDos.forEach((toDo) => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", deleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
-
-store.subscribe(paintToDo);
-
-const onSubmit = (e) => {
-  e.preventDefault();
-
-  const toDo = input.value;
-  input.value = "";
-  AddToDo(toDo);
-};
-
-form.addEventListener("submit", onSubmit);
+ReactDOM.render(<App></App>, document.getElementById("root"));
